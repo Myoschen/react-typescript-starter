0ec7ef9f88867226bfe90a3317e428ec993d1766
Myos <68583272+willy14620@users.noreply.github.com>
2022-03-30 19:51:06 +0800

    build: enable plugins and assets module

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: {
+    main: path.resolve(__dirname, './src/index.tsx'),
+  },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    assetModuleFilename: 'static/images/[name].[ext]',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
@@ -23,16 +27,23 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'static/css/[name].css',
     }),
     new HtmlWebpackPlugin({
+      title: 'React, TypeScript Starter Kit',
       template: './public/index.html',
       filename: 'index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
   devServer: {
     port: 3000,
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,7 @@
 const { merge } = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
-const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-dependencies
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
 });
