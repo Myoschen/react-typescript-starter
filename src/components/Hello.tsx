fcd5e08c82a4c0db69a9708155f277c2d09f2fd3
Myos <68583272+willy14620@users.noreply.github.com>
2022-03-30 19:50:17 +0800

    add: banner image

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import banner from '../assets/images/test.png';
 
 export interface HelloProps {
   compiler: string;
@@ -8,16 +9,19 @@ export interface HelloProps {
 export function Hello(props: HelloProps) {
   const { compiler, framework } = props;
   return (
-    <h1>
-      Hello from
-      {' '}
-      {compiler}
-      {' '}
-      and
-      {' '}
-      {framework}
-      {' '}
-      !
-    </h1>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <img alt="banner" style={{ width: '100%' }} src={banner} />
+      <h1 style={{}}>
+        Hello from
+        {' '}
+        {compiler}
+        {' '}
+        and
+        {' '}
+        {framework}
+        {' '}
+        !
+      </h1>
+    </div>
   );
 }
